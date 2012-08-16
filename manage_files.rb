eafb3006e3c9a2177221b838543ae03b573cb6a6
Ryan Breidenbach <ryan.breidenbach+github@gmail.com>
2012-08-15 20:12:01 -0500

    Created cache for deleted and skipped files.

--- manage_files.rb
+++ manage_files.rb
@@ -4,13 +4,12 @@ require 'fileutils'
 require 'yaml'
 
 def consolidate_files(pandora_jam)
-  Dir["#{pandora_jam}/*/**"].each do | file |
-    file_name = file.split("/").last
-    target = "#{pandora_jam}/#{file_name}"
-    if File.exists? target
-      File.delete file
+  Dir["#{pandora_jam}/*/**"].each do | from |
+    to = "#{pandora_jam}/#{File.basename(from)}"
+    if File.exists? to
+      File.delete from
     else
-      FileUtils.move file, target
+      FileUtils.move from, to
     end
   end
 end
@@ -45,10 +44,22 @@ def pick_songs(pandora_jam, target)
   end
 end
 
+def load_cache_file(file_name)
+  if File.exists? file_name
+    return IO.readlines file_name
+  else
+    File.new file_name, 'w'
+    return []
+  end
+end
+
 config = YAML.load_file("config/directories.yml")
 
 pandora_jam = config['pandora_jam']
 target = config['target']
+
+skipped_cache = load_cache_file "config/.skipped"
+deleted_cache = load_cache_file "config/.deleted"
 
 consolidate_files pandora_jam
 remove_duplicates pandora_jam, target
